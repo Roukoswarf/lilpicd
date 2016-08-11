b741ca3ea89d738e09ad2f51b3edf2763e49c826
moscato <thescottrosenberg@gmail.com>
2016-08-11 14:01:49 -0500

    Added compressutil and compressargs

--- config.py
+++ config.py
@@ -5,17 +5,54 @@ watchdir = './img'
 # options are: [int], 'auto'
 threads = 1
 
-compressutils = {'png': ['zopfli'],
-				 'jpeg': ['jpegtran'],
-				 'gif': ['gifsicle'],
-				 }
-compressargs = {'zopfli': ['-z 4'],
-				'jpegtran': ['-optimize', '-trim', '-progressive'],
-				'gifsicle': ['--optimize']
+compressutils = {'png' :[
+						'zopflipng',
+						#'optipng',
+						#'advdef',#redundant with zopflipng or advpng, this tool is part of advancecomp
+						#'advpng',#redundant with zopflipng or advdef
+						'exiftool'
+						]
+
+				'jpeg' :[
+						 'jpegtran',
+						 #'cjpeg',#redundant with jpegtran, this tool is part of mozjpeg
+						 'exiftool'
+						 ] 
+				
+				'gif'  :[
+						 'gifsicle',
+						 'exiftool'
+						 ]
 				}
+compressargs = {
+				'gifsicle' :[
+							'-b',
+							'-o3'
+				]
 
-#advpng -z 4
-
-#jpegtran -optimize -trim -progressive
-
-#gifscile  --optimize
+				'exiftool' :['-all=']
+				
+				'advdef'   :['-z4']					
+													
+				'advpng'   :['-z4']					
+				
+				'optipng'  :['-o4']
+				
+				'zopflipng':[
+							 #'-m',			   		#this is doubles time
+							 #'--splitting=3', 		#this is doubles time
+							 '--lossy_transparent'  #loses invisible data
+							 '--prefix'				#mandatory
+							 ]			
+							 
+				'jpegtran' :[
+							 '-optimize',
+							 '-trim',				#nearly perfect but technically lossy
+							 '-progressive'
+							 ]				
+				'cjpeg'    :[
+							 '-optimize', 			
+							 '-quality 90', 		#this is lossy, defaults to 75 if not set
+							 '-progressive'
+							 ]
+				}
